e8b686b15c4ebd2d7a6e58a5dd5b4d0e4d7645e3
Akhil krishnan m <akhilkrishnan3860645@gmail.com>
2023-11-20 18:23:59 +0530

    validation done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 let firstNameError = document.getElementById("f-nameErr");
 let lastNameError = document.getElementById("l-nameErr");
 let emailError = document.getElementById("emailErr");
@@ -30,12 +29,16 @@ function validateMailAndNumber() {
 
     if (email.length === 0) {
         emailError.innerHTML = "Enter your email";
+    } else if (!isValidEmail(email)) {
+        emailError.innerHTML = "Enter a valid email address";
     } else {
         emailError.innerHTML = "";
     }
 
     if (number.length === 0) {
         numberError.innerHTML = "Enter your phone number";
+    } else if (!isValidPhoneNumber(number)) {
+        numberError.innerHTML = "Enter a valid 10-digit phone number";
     } else {
         numberError.innerHTML = "";
     }
@@ -43,17 +46,42 @@ function validateMailAndNumber() {
     return email.length > 0 && number.length > 0;
 }
 
-// Example of how to use these functions in a form submission:
-function validateForm(event) {
-    
+function isValidEmail(email) {
+    // Use a regular expression to check if the email format is valid
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function isValidPhoneNumber(number) {
+    // Check if the phone number consists of exactly 10 digits
+    return /^\d{10}$/.test(number);
+}
+
+function submitForm() {
     let isNameValid = validateName();
     let isMailAndNumberValid = validateMailAndNumber();
 
     if (!isNameValid || !isMailAndNumberValid) {
-        submitError.innerHTML = "Please fill in all required fields.";
-        return false;
-    }
+        submitError.innerHTML = "Please fill in all required fields correctly.";
+    } else {
+        // Perform any additional actions you need for a successful form submission
+        submitError.innerHTML = "submitted successfully!";
+        // Optionally, refresh the page after a delay (e.g., 2 seconds)
+        submitError.style.color = "green";
 
-    submitError.innerHTML = "";
-    return true;
+        setTimeout(() => {
+            location.reload();
+        }, 3000);
+
+    }
 }
+
+// Example of how to use these functions in a form submission:
+function validateForm(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    submitForm();
+}
+
+// Attach the validateForm function to the form's submit event
+document.getElementById("form").addEventListener("submit", validateForm);
